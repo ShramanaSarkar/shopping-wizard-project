6b499a0c7e904c50d6bd3281fb0fa4bccb384f27
ShramanaSarkar <shramana2000@gmail.com>
2025-04-12 15:37:41 +0530

    router updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import ThemeSelection from './pages/ThemeSelection';
 import ProductTypePage from './pages/ProductTypePage';
 import AddProduct from './pages/AddProduct';
