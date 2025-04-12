5927184cfaae907aa1c62e216430535b6baf6348
ShramanaSarkar <55043212+ShramanaSarkar@users.noreply.github.com>
2025-04-12 11:32:14 +0530

    Theme added for product

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ThemeSelection from './pages/ThemeSelection';
 import ProductTypePage from './pages/ProductTypePage';
 import AddProduct from './pages/AddProduct';
 import DashboardPage from './pages/DashboardPage';
 import EditProduct from './pages/EditProduct';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [theme, setTheme] = useState(null);
@@ -27,7 +27,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ThemeSelection theme={theme} setTheme={setTheme} />} />
         <Route path="/product-type" element={<ProductTypePage setProductType={setProductType} setCategory={setCategory} setSubcategory={setSubcategory} />} />
-        <Route path="/add-product" element={<AddProduct countProduct={countProduct} setCountProduct={setCountProduct} addProduct={addProduct} productType={productType} category={category} subcategory={subcategory} />} />
+        <Route path="/add-product" element={<AddProduct theme={theme} countProduct={countProduct} setCountProduct={setCountProduct} addProduct={addProduct} productType={productType} category={category} subcategory={subcategory} />} />
         <Route path="/dashboard" element={<DashboardPage products={products} theme={theme} />} />
         <Route path="/edit-product/:productId" element={<EditProduct products={products} updateProduct={updateProduct} />} />
       </Routes>
